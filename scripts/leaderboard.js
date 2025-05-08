39d9d3855006c8cefaace166e65b1e6d73ebd313
Alex Savelev <>
2025-05-08 19:33:00 +0300

    Leaderboard receive v1

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -45,6 +45,21 @@ export class LeaderboardManager {
   }
 
   receiveLeaderboardRecords() {
+    const url = '/api/records';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log(json);
+
+    } catch (error) {
+      console.error(error.message);
+    }
+
+
     // TODO: recieve
     const leaderboardData = {
       'Level1': [
